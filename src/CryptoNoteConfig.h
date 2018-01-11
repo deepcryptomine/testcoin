ef4fedb7d47edf58d23d2eaaa81968d5be5862cb
deepcryptomine <deepcryptomine@gmail.com>
2018-01-10 23:29:38 -0800

    Added seed node

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "10.0.2.15:8080",
+  "192.168.0.104",
   "127.0.0.1:8080",
 };
 
