7918cb853a5f4bcf9a83457b5338ce092e100ffb
deepcryptomine <deepcryptomine@gmail.com>
2018-01-12 20:35:30 -0800

    Added two VPS as seed nodes. Modified total supply and network identifier.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "DeepCoin";
-const char     GENESIS_COINBASE_TX_HEX[] 		     = "";
+const char     GENESIS_COINBASE_TX_HEX[] 		     = "013c01ff0001e61d029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121015cc650d8d0336f96a0035c0800bd456f8390a00898a52b9d8d34ac136d13b356";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
